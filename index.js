c0d6f73eed79e110e8136d6653052f0c8fce57b2
orasa oneal <orasaoneal@gmail.com>
2019-07-21 04:30:21 -0700

    install express, reqire on index.js, add app.listen, add first api

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+//install express and import it here
+//install dotenv
+//assign express to app so we can use it to create route
+const express = require('express')
+require('dotenv').config()
+const app = express()
 
+require('./db')
+
+//create route
+app.post('api/category', require('./controllers/category'))
 
 
 
@@ -12,5 +22,7 @@
 app.listen(process.env.PORT, (err) => {
 	if (err) {
 		console.log('ERROR:', err)
+  } else {
+    console.log('Listen on port ${process.env.PORT}')
   }
 }
