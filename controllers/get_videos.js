a58914ced9ab6ae8ed8e20de446049a5f755a02d
orasa oneal <orasaoneal@gmail.com>
2019-07-21 21:28:37 -0700

    populate video with category and user to select and show certain element

--- controllers/get_videos.js
+++ controllers/get_videos.js
@@ -1,7 +1,13 @@
 const db_video = require('../models/video')
 
 module.exports = (req, res) => {
-	db_video.find({}).then((data) => {
+	db_video.find({}).populate({
+		path: 'category',
+		select: 'name'
+	}).populate({
+		path: 'user',
+		select: 'name email'
+	}).then((data) => {
 		res.send(data)
 	}).catch((err) => {
 		res.send(err)
