489932e5e0443512ebadb8360d88efb6c50116f5
orasa oneal <orasaoneal@gmail.com>
2019-07-22 02:09:10 -0700

    math password, create token with jwt.sign()

--- controllers/login.js
+++ controllers/login.js
@@ -1,20 +1,27 @@
-
 const db_user = require('../models/user')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+//findOne() function to find if email exsist in our db.,
+//pass user with that email as param, if user exist with email from request,
+//then match password using bcrypt compare(),
+//if password match use jwt sign() and convert user to object.
 
 
 module.exports = (req, res) => {
 	console.log(req.body)
-
-	// 1. Check if email exists in db , pass param user
+	// 1. Check if email exists in db
 	db_user.findOne({email: req.body.email}).then((user) => {
-		//if user exist
 		if (user) {
-		// 2. If email found, match passwords from client request login with password in db
+			// 2. If email found, match passwords
 			bcrypt.compare(req.body.password, user.password, (err, match) => {
 				if (match) {
 					// 4. If passwords match, res OK
-					res.send('You are logged in')
+					let token = jwt.sign(user.toObject(), '123')
+					res.status(200).json({
+						message: 'Congrat! you are logged in',
+						token: token
+					})
 				} else {
 					res.send('Sorry, invalid password')
 				}
@@ -27,27 +34,3 @@ module.exports = (req, res) => {
 	})
 
 }
-
-
-
-
-
-
-
-// 	bcrypt.compare(req.body.password, db_user.find(db_user.password), (err, match) => {
-// 		//1.check if password match to the one in the database
-// 		if (match) {
-// 			 // 2. If passwords matched, then use jwt sign function to convert the user info into token
-// 			res.send('You are logged in')
-// 			let token = jwt.sign(user.toObject(), '123')
-//
-// 			//use respons jason send respons back to client both massage and token at the same time
-// 			res.json({
-// 				message: 'Congrat!, You are logged in',
-// 				token: token
-// 			})
-// 		} else {
-// 			res.send('Invalid password')
-// 		}
-// 	})
-// }
