44cb325bbfa802a1a3cd1d211a5fbfaf87b2c127
orasa oneal <orasaoneal@gmail.com>
2019-07-22 05:44:52 -0700

    add decript token to decode and use as user on post video

--- controllers/post_video.js
+++ controllers/post_video.js
@@ -1,9 +1,37 @@
 const db_video = require('../models/video')
+const jwt = require('jsonwebtoken')
 
+
+//add decript token and use as author on video create(post video)
 module.exports = (req, res) => {
-	db_video.create(req.body).then((data) => {
-		res.send(data)
-	}).catch((err) => {
-		res.send(err)
+
+	//jwt.verify(take 3 params)
+
+console.log('req.body', req.body)
+
+	jwt.verify(req.body.token, '123', (err, decoded) => {
+		if (decoded) {
+			console.log('decoded', decoded)
+			req.body.user = decoded._id
+			db_video.create(req.body).then((data) => {
+				res.send(data)
+			}).catch((err) => {
+				res.send(err)
+			})
+		}
 	})
 }
+
+
+
+
+
+
+//Basic post video
+// module.exports = (req, res) => {
+// 	db_video.create(req.body).then((data) => {
+// 		res.send(data)
+// 	}).catch((err) => {
+// 		res.send(err)
+// 	})
+// }
