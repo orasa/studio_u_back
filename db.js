44f8eee489ed277154b2526e236a335d9de90f8f
orasa oneal <orasaoneal@gmail.com>
2019-07-22 22:34:21 -0700

    move db to .env file

--- db.js
+++ db.js
@@ -4,6 +4,8 @@ const mongoose = require ('mongoose')
 //connecting to mongodb use mongoose to connect look at .env file
 // variable DATABASE_URL=mongodb://localhost:27017/unicorns so we can use process her
 
+// mongoose.connect('mongodb://localhost:27017/slack03', {useNewUrlParser: true}, (err) => {
+//move db to .env
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true}, (err) => {
 	if (err) {
