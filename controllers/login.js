f3767511369d6ab3db5d35b936d144691e8de1fb
orasa oneal <orasaoneal@gmail.com>
2019-07-30 21:11:36 -0700

    update signUp and Login

--- controllers/login.js
+++ controllers/login.js
@@ -1,36 +1,37 @@
-const db_user = require('../models/user')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const db_user = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 //findOne() function to find if email exsist in our db.,
 //pass user with that email as param, if user exist with email from request,
 //then match password using bcrypt compare(),
 //if password match use jwt sign() and convert user to object.
 
-
 module.exports = (req, res) => {
-	console.log(req.body)
+	console.log(req.body);
 	// 1. Check if email exists in db
-	db_user.findOne({email: req.body.email}).then((user) => {
-		if (user) {
-			// 2. If email found, match passwords
-			bcrypt.compare(req.body.password, user.password, (err, match) => {
-				if (match) {
-					// 4. If passwords match, res OK
-					let token = jwt.sign(user.toObject(), process.env.SECRET)
-					res.status(200).json({
-						message: 'Congrats! you are logged in',
-						token: token
-					})
-				} else {
-					res.send('Sorry, invalid password')
-				}
-			})
-		} else {
-			res.send('Sorry, email not found')
-		}
-	}).catch((err) => {
-		res.status(300).send(err)
-	})
-
-}
+	db_user
+		.findOne({ email: req.body.email })
+		.then(user => {
+			if (user) {
+				// 2. If email found, match passwords
+				bcrypt.compare(req.body.password, user.password, (err, match) => {
+					if (match) {
+						// 4. If passwords match, res OK
+						let token = jwt.sign(user.toObject(), process.env.SECRET);
+						res.status(200).json({
+							message: 'Congrats! you are logged in',
+							token: token
+						});
+					} else {
+						res.send('Sorry, invalid password');
+					}
+				});
+			} else {
+				res.send('Sorry, email not found');
+			}
+		})
+		.catch(err => {
+			res.status(300).send(err);
+		});
+};
--- controllers/signup.js
+++ controllers/signup.js
@@ -1,27 +1,28 @@
-const db_user = require('../models/user')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const db_user = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res) => {
 	bcrypt.hash(req.body.password, 10, (err, encrypted) => {
 		if (err) {
-			res.status(300).send('Error:', err)
+			res.status(300).send('Error:', err);
 		} else {
-			req.body.password = encrypted
-			db_user.create(req.body).then((user) => {
-				let token = jwt.sign(user.toObject(), process.env.SECRET)
-				res.status(200).json({
-					message: 'You are signed up',
-					token: token
+			req.body.password = encrypted;
+			db_user
+				.create(req.body)
+				.then(user => {
+					let token = jwt.sign(user.toObject(), process.env.SECRET);
+					res.status(200).json({
+						message: 'You are signed up, Yo!',
+						token: token
+					});
 				})
-			}).catch((err) => {
-				res.send(err)
-			})
+				.catch(err => {
+					res.send(err);
+				});
 		}
-	})
-}
-
-
+	});
+};
 
 //before encryption basic signup
 
