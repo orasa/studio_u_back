d6b6d03715ce09fd9174e0e1db69fd451323688c
orasa oneal <orasaoneal@gmail.com>
2019-07-30 21:19:30 -0700

    delete unneccessary files

--- controllers/get_choices.js
+++
@@ -1,9 +0,0 @@
-const db_category = require('../models/category')
-
-module.exports = (req, res) => {
-	db_category.find({}).then((data) => {
-		res.send(data)
-	}).catch((err) => {
-		res.send(err)
-	})
-}
--- controllers/post_choice.js
+++
@@ -1,9 +0,0 @@
-const db_choice = require('../models/choice')
-
-module.exports = (req, res) => {
-	db_choice.create(req.body).then((data) => {
-		res.send(data)
-	}).catch((err) => {
-		res.send(err)
-	})
-}
